f78c9736e8a4d675249bf0fd02bc2300711b0b08
David Mouton <damo@tamina-online.com>
2014-01-23 14:15:43 +0100

    + GPS convert

--- Player/src/com/tamina/bikewar/data/vo/BikeStationVO.hx
+++ Player/src/com/tamina/bikewar/data/vo/BikeStationVO.hx
@@ -12,8 +12,11 @@ class BikeStationVO {
     public var riveDroite:String;
     public var rocade:String;
 
+    public static var northEastGPS:Point;
+    public static var southWestGPS:Point;
 
-    public function new(id:String,name:String,numSlot:String,latitude:String,longitude:String,type:String,CB:String,zone:String,riveDroite:String,rocade:String) {
+
+    public function new(id:String,name:String,numSlot:String,latitude:String,longitude:String,type:String,CB:String,zone:String,riveDroite:String,rocade:String="In") {
         this.id = id;
         this.name = name;
         this.numSlot = Std.parseInt( numSlot );
@@ -24,12 +27,14 @@ class BikeStationVO {
         this.zone = Std.parseInt(zone);
         this.riveDroite = riveDroite;
         this.rocade = rocade;
+        northEastGPS = new Point(-0.618948,44.862166);
+        southWestGPS = new Point(-0.508407,44.829026);
 
     }
 
     public function toBikeStation(width:Int, height:Int):BikeStation{
         var result:BikeStation = new BikeStation();
-        result.position = new Point( Math.round(Math.random() * width), Math.round(Math.random() * height));
+        result.position = new Point( Math.round(width * ( this.longitude - northEastGPS.x ) / ( southWestGPS.x - northEastGPS.x ))-17, Math.round( height * ( northEastGPS.y - this.latitude ) / ( northEastGPS.y - southWestGPS.y ))-25 );
         result.slotNum = this.numSlot;
         result.bikeNum = Math.round(Math.random() * result.slotNum);
         for (i in 0...96) {
